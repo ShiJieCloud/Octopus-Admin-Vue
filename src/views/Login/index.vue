eb0faa6d083c52c86a143b2c858b72b1b3cd98e4
赵士杰 <1500492856@qq.com>
2024-11-22 11:10:04 +0800

    补充登录动画

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -24,24 +24,28 @@ const excludedKeepAliveComponents = ['QRCODE']
 </script>
 
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <div class="lg:w-1/2 h-full bg-contain bg-center bg-[url('@/assets/images/login.svg')] bg-no-repeat" />
-    <div class="w-full lg:w-2/5 h-full flex flex-col items-center justify-center">
-      <!--   LOGO   -->
-      <div class="flex flex-col items-center justify-center mb-4 space-y-2">
-        <SvgIcon iconName="logo" :iconSize="24" />
-        <span class="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-          {{ AppConfig.getAppTitle() }}
-        </span>
-      </div>
+  <div class="w-full h-full xl:bg-gray-200 flex justify-center items-center">
+    <div class="w-full xl:w-5/6 h-full xl:h-5/6 xl:rounded-xl xl:drop-shadow-md bg-white flex
+       justify-center items-center transition-all duration-500 ease-in-out"
+    >
+      <div class="lg:w-1/2 h-full bg-contain bg-center bg-[url('@/assets/images/login.svg')] bg-no-repeat" />
+      <div class="w-full lg:w-2/5 h-full flex flex-col items-center justify-center">
+        <!--   LOGO   -->
+        <div class="flex flex-col items-center justify-center mb-4 space-y-2">
+          <SvgIcon iconName="logo" :iconSize="24" />
+          <span class="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+            {{ AppConfig.getAppTitle() }}
+          </span>
+        </div>
 
-      <!--   登录表单   -->
-      <div class="w-full flex items-center justify-center max-w-xs md:max-w-sm">
-        <Transition name="fade">
-          <keep-alive :exclude="excludedKeepAliveComponents">
-            <component :is="loginMode" :key="themeStore.loginMode" />
-          </keep-alive>
-        </Transition>
+        <!--   登录表单   -->
+        <div class="w-full flex items-center justify-center max-w-xs md:max-w-sm">
+          <Transition name="fade">
+            <keep-alive :exclude="excludedKeepAliveComponents">
+              <component :is="loginMode" :key="themeStore.loginMode" />
+            </keep-alive>
+          </Transition>
+        </div>
       </div>
     </div>
   </div>
@@ -61,17 +65,23 @@ const excludedKeepAliveComponents = ['QRCODE']
 }
 
 /* 过渡类 */
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 0.3s ease, transform 0.3s ease;
+.fade-enter-active,
+.fade-leave-active {
+  transition:
+    opacity 0.3s ease,
+    transform 0.3s ease;
 }
 
-.fade-enter, .fade-leave-to {
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
   transform: translateY(20px);
 }
 
 .fade-leave-active {
-  transition: opacity 0s ease, transform 0s ease;
+  transition:
+    opacity 0s ease,
+    transform 0s ease;
 }
 
 /* 自定义动画效果 */
