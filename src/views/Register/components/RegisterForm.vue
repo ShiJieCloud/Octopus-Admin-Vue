c93f775ec8f219b1b14c3aebce738f8062a07a41
赵士杰 <1500492856@qq.com>
2024-11-22 21:04:04 +0800

    添加注册表单校验规则

--- src/views/Register/components/RegisterForm.vue
+++ src/views/Register/components/RegisterForm.vue
@@ -1,17 +1,79 @@
-<script setup lang="ts" name="RegisterForm"></script>
+<script setup lang="ts" name="RegisterForm">
+import { reactive, ref } from 'vue'
+import { FormInstance, FormRules } from 'element-plus'
+import { RegisterFormData } from '@/types/register'
+
+const registerFormRef = ref<FormInstance>()
+
+const registerForm = ref<RegisterFormData>({
+  username: '',
+  phone: '',
+  captcha: '',
+  password: '',
+  confirmPassword: '',
+})
+
+// 自定义校验规则，检验确认密码是否一致
+const validateConfirmPassword = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('请确认密码'))
+  } else if (value !== registerForm.value.password) {
+    callback(new Error('两次密码输入不一致，请重新输入'))
+  } else {
+    callback()
+  }
+}
+
+// 校验规则
+const registerFormRules = reactive<FormRules<RegisterFormData>>({
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 3, max: 15, message: '用户名长度在 3 到 15 个字符之间', trigger: 'blur' },
+  ],
+  phone: [
+    { required: true, message: '请输入手机号', trigger: 'blur' },
+    { pattern: /^1[3-9]\d{9}$/, message: '请输入有效的手机号', trigger: 'blur' },
+  ],
+  captcha: [
+    { required: true, message: '请输入验证码', trigger: 'blur' },
+    { min: 4, max: 6, message: '验证码长度应为 4 到 6 位', trigger: 'blur' },
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 6, max: 20, message: '密码长度应在 6 到 20 位之间', trigger: 'blur' },
+  ],
+  confirmPassword: [
+    { required: true, message: '请确认密码', trigger: 'blur' },
+    {
+      validator: validateConfirmPassword,
+      trigger: 'blur',
+    },
+  ],
+})
+
+const handleRegister = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  // 调用 form.validate 方法触发表单验证
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!')
+    }
+  })
+}
+</script>
 
 <template>
   <div class="w-full h-full">
-    <el-form ref="ruleFormRef" size="large">
+    <el-form ref="registerFormRef" :model="registerForm" :rules="registerFormRules" size="large">
       <el-form-item prop="username">
-        <el-input placeholder="用户名" clearable>
+        <el-input placeholder="用户名" v-model="registerForm.username" clearable>
           <template #prefix>
             <SvgIcon iconName="user" />
           </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="phone">
-        <el-input placeholder="手机号码">
+        <el-input placeholder="手机号码" v-model="registerForm.phone">
           <template #prefix>
             <SvgIcon iconName="phone" />
           </template>
@@ -19,7 +81,7 @@
       </el-form-item>
       <el-form-item prop="captcha">
         <div class="flex items-center w-full gap-4">
-          <el-input placeholder="请输入验证码" class="w-2/3">
+          <el-input placeholder="请输入验证码" v-model="registerForm.captcha" class="w-2/3">
             <template #prefix>
               <SvgIcon iconName="captcha" iconClass="size-5" />
             </template>
@@ -30,21 +92,21 @@
         </div>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input placeholder="密码" type="password" show-password clearable>
+        <el-input placeholder="密码" type="password" v-model="registerForm.password" show-password clearable>
           <template #prefix>
             <SvgIcon iconName="password" />
           </template>
         </el-input>
       </el-form-item>
-      <el-form-item prop="password">
-        <el-input placeholder="确认密码" type="password" show-password clearable>
+      <el-form-item prop="confirmPassword">
+        <el-input placeholder="确认密码" type="password" v-model="registerForm.confirmPassword" show-password clearable>
           <template #prefix>
             <SvgIcon iconName="password" />
           </template>
         </el-input>
       </el-form-item>
       <el-form-item>
-        <el-button class="w-full" type="primary"> 点击注册</el-button>
+        <el-button class="w-full" type="primary" @click="handleRegister(registerFormRef)"> 点击注册</el-button>
         <div class="leading-6 flex w-full justify-center">
           <el-link class="text-md" type="primary" href="/login"> 已有账号？去登录</el-link>
         </div>
